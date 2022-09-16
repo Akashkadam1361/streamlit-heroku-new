30be2450b67ab1e3039ff622ed861f2346fa6edd
Akash Kadam <96363861+Akashkadam1361@users.noreply.github.com>
2022-09-16 22:39:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ number2 = st.number_input('Insert second number',value=0)
 if number1 is not None and number2 is not None:
     st.write("Sum of the numbers is: {}".format((number1+number2)))
     st.write("Subtraction of the numbers is: {}".format((number1-number2)))
-    st.write("division of the numbers is: {}".format((number1/number2)))
     st.write("multiplication of the numbers is: {}".format((number1*number2)))
+if number2 > 0:
+    st.write("division of the numbers is: {}".format((number1/number2)))
 
